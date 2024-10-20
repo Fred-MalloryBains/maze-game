2cea598ebfedab1fbeb4bd7d0443ef53bc36255d
Fred-MalloryBains <146655395+Fred-MalloryBains@users.noreply.github.com>
2024-10-20 17:04:40 +0100

    adding to repository

---
+++ game/maze 2.h
@@ -0,0 +1,28 @@
+#ifndef maze_h
+#define maze_h
+
+// defines for max and min permitted dimensions
+#define MAX_DIM 101
+#define MIN_DIM 4
+
+#define EXIT_SUCCESS 0
+#define EXIT_ARG_ERROR 1
+#define EXIT_FILE_ERROR 2
+#define EXIT_MAZE_ERROR 3
+
+typedef struct __Coord
+{
+    int x;
+    int y;
+} coord;
+
+typedef struct __Maze
+{
+    char **map;
+    int height;
+    int width;
+    coord start;
+    coord end;
+} maze;
+
+#endif
